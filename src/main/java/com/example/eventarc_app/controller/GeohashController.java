a8e4169fde1b149b06ccf809ac4a56549503e357
PraveenJJ <praveen_jj123@yahoo.com>
2025-07-27 22:55:28 +0530

    update apis to support realtime event payload

--- src/main/java/com/example/eventarc_app/controller/GeohashController.java
+++ src/main/java/com/example/eventarc_app/controller/GeohashController.java
@@ -1,13 +1,14 @@
 package com.example.eventarc_app.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
+import com.google.events.cloud.firestore.v1.DocumentEventData;
+import com.google.events.cloud.storage.v1.StorageObjectData;
+import com.google.protobuf.InvalidProtocolBufferException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.BufferedReader;
 
 @RestController
 @RequestMapping("/tmsgis/geohash")
@@ -15,22 +16,13 @@ public class GeohashController {
 
     private static final Logger logger = LoggerFactory.getLogger(GeohashController.class.getName());
 
-    @PostMapping("/event")
-    public void handleFirestoreEvent(HttpServletRequest request) {
-        try {
-            StringBuilder sb = new StringBuilder();
-            String line;
-            BufferedReader reader = request.getReader();
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-            }
-            String requestBody = sb.toString();
-            logger.info("Request Body Start");
-            logger.info(requestBody);
-            logger.info("Request Body End");
-        } catch (Exception e) {
-            logger.error("Error reading request body: {}", e.getMessage());
-        }
+    // for intercepting events from firestore
+    @PostMapping(value = "/event", consumes = "application/protobuf")
+    public void handleFirestoreEvent(@RequestBody byte[] data) throws InvalidProtocolBufferException {
+        DocumentEventData firestoreEventData = DocumentEventData.parseFrom(data);
+        logger.info("Firestore Event Start");
+        logger.info(firestoreEventData.toString());
+        logger.info("Firestore Event End");
     }
 
 }
--- src/main/java/com/example/eventarc_app/controller/LoaderController.java
+++ src/main/java/com/example/eventarc_app/controller/LoaderController.java
@@ -1,13 +1,14 @@
 package com.example.eventarc_app.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
+import com.google.events.cloud.storage.v1.StorageObjectData;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.BufferedReader;
 
 @RestController
 @RequestMapping("/tmsgis/loader")
@@ -15,22 +16,15 @@ public class LoaderController {
 
     private static final Logger logger = LoggerFactory.getLogger(LoaderController.class.getName());
 
+    // for intercepting events from cloud storage
     @PostMapping("/process")
-    public void processFile(HttpServletRequest request) {
-        try {
-            StringBuilder sb = new StringBuilder();
-            String line;
-            BufferedReader reader = request.getReader();
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-            }
-            String requestBody = sb.toString();
-            logger.info("Request Body Start");
-            logger.info(requestBody);
-            logger.info("Request Body End");
-        } catch (Exception e) {
-            logger.error("Error reading request body: {}", e.getMessage());
-        }
+    public void processFile(@RequestBody String jsonData) throws InvalidProtocolBufferException {
+        logger.info("Cloud Storage Event Start");
+        StorageObjectData.Builder builder = StorageObjectData.newBuilder();
+        JsonFormat.parser().merge(jsonData, builder);
+        StorageObjectData storageObjectData = builder.build();
+        logger.info(storageObjectData.toString());
+        logger.info("Cloud Storage Event End");
     }
 
 }
